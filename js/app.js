c717690352f447dbaa3f0b94fa575fbb0ec01e0f
JRKreutzer <jose.kreutzer@gmail.com>
2023-10-11 18:08:44 -0300

    Obter os valores

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function adicionar() {
+
+    let selecao = document.getElementById('produto');
+	console.log(selecao.value); // Português
+    let quantidade = document.getElementById("quantidade");
+    console.log(quantidade.value); // Português
+
+
+}
+
+function limpar() {
+
+}
